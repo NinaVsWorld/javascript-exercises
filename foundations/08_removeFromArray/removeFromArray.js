3a95f82445a940aeb63f14d9fe7c8278520edd5b
NinaVsWorld <130551356+NinaVsWorld@users.noreply.github.com>
2026-01-13 23:20:08 +1100

    Complete exercises 6-8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...manyMoreArgs) {
+    return arr.filter(item =>
+        !manyMoreArgs.includes(item)
+    );
+}
+
+console.log(removeFromArray([1, 2, 3, 4], 7, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
