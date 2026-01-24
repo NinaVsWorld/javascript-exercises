2db40feb20547172e20b4f31e8836c41bf52a174
NinaVsWorld <130551356+NinaVsWorld@users.noreply.github.com>
2026-01-24 14:06:19 +1100

    Complete fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const n = parseInt(num);
+    if (n < 0) {
+        return "OOPS";
+    }
+    return helper(0, 1, n, 0);
 };
+
+const helper = function(prev, next, stop, acc) {
+    if (acc == stop) {
+        return prev;
+    }
+    const num = prev + next;
+    return helper(next, num, stop, acc + 1);
+}
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
