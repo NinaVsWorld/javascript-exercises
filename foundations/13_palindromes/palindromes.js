550595ccb8481469710b05d67ca3d751b6729a81
NinaVsWorld <130551356+NinaVsWorld@users.noreply.github.com>
2026-01-24 13:17:56 +1100

    complete palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const arr = str.toLowerCase().split('').filter(letter => {
+        return !/[.,!?;:"'(){}[\]-]/.test(letter);
+    }).filter(letter => {
+        return /\S/.test(letter);
+    });
+    const reversedArr = str.toLowerCase().split('').reverse().filter(letter => {
+        return !/[.,!?;:"'(){}[\]-]/.test(letter);
+    }).filter(letter => {
+        return /\S/.test(letter);
+    });
+    return arr.every((letter, index) => letter === reversedArr[index]);
 };
+console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
